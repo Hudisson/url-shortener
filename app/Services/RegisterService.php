891cc7e22abf4b8014b0b163babcb110e4306dbc
Hudisson Xavier <hudissonxavier2018@gmail.com>
2026-08-23 12:29:43 -0300

    feat: implementar geração de código de verificação

--- app/Services/RegisterService.php
+++ app/Services/RegisterService.php
@@ -8,12 +8,21 @@ use App\Models\User;
 
 final readonly class RegisterService
 {
+
+    public function __construct(
+        private EmailVerificationService $emailVerificationService
+    ){}
+
     public function create(string $name, string $email, string $password): User
     {
-        return User::create([
+        $user =  User::create([
             'name' => $name,
             'email' => $email,
             'password' => $password,
         ]);
+
+        $this->emailVerificationService->createFor($user);
+
+        return $user;
     }
 }
